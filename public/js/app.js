337bcbc25892f4ccf593fbac2a09506020e79a57
Daniel Hebestreit <daniel.hebestreit@gmail.com>
2016-02-25 21:11:55 +0100

    added angular routing cleaned up templates

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,17 @@
-var app = angular.module('todoApp', ['ngMaterial'])
-    .config(function ($mdThemingProvider) {
+var app = angular.module('todoApp', ['ngMaterial', 'ngRoute'])
+    .config(function ($routeProvider, $locationProvider, $mdThemingProvider) {
+        $locationProvider.html5Mode(true);
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/app.html',
+                controller: 'MainController'
+            })
+            .when('/categories', {
+                templateUrl: 'views/categories/index.html',
+                controller: 'CategoriesController'
+            }).otherwise({redirectTo: '/'});
+
         $mdThemingProvider
             .theme('default')
             .primaryPalette("indigo")
---
+++ public/js/controller/CategoriesController.js
@@ -0,0 +1,2 @@
+app.controller('CategoriesController', function ($scope) {
+});
